70ba1a7668808d4f88475f07906b9a960969c462
SUZUKI Kei <todokimasen@gmail.com>
2018-05-14 17:37:21 +0900

    tasks/ 以下のファイルを自動的に読み込むように変更.

--- app/tasks.py
+++ app/tasks.py
@@ -1,11 +1,18 @@
-import os, sys, importlib
+import sys
+from glob import glob
+from importlib import import_module
+from os import path
+
 import invoke
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "tasks"))
+def import_modules(directory_path):
+    if directory_path not in sys.path:
+        sys.path.append(directory_path)
 
-namespace = invoke.Collection(*map(importlib.import_module, (
-    "app",
-    "test",
-    "version",
-)))
+    file_paths = glob(path.join(directory_path, "[a-zA-Z0-9]*.py"))
+    file_names = map(path.basename, file_paths)
+    module_names = [path.splitext(file_name)[0] for file_name in file_names]
+    return map(import_module, module_names)
+
+namespace = invoke.Collection(*import_modules(path.join(path.dirname(__file__), "tasks")))
 
