a54cf2c4ddf49802269a7dde573e0fda540549dc
SUZUKI Kei <todokimasen@gmail.com>
2018-06-03 12:58:07 +0900

    テスト実行を外部コマンド呼び出しではなく Python コードで行うように変更.

--- app/tasks/test.py
+++ app/tasks/test.py
@@ -1,21 +1,29 @@
+import os
+from unittest import TestLoader
+from unittest import TextTestRunner
+
+from coverage import Coverage
 from invoke import task
 
+def _run_unit_tests():
+    test_suite = TestLoader().discover("/app/src/test/python", "test_*.py", ".")
+    TextTestRunner().run(test_suite)
+
 @task(name="unit", default=True)
-def run_unit_test(context):
+def run_unit_tests(context):
     """単体テストを実行する."""
-    context.run("python -m unittest discover -s /app/src/test/python/myapp/ -t .")
+    _run_unit_tests()
 
 @task(name="coverage")
 def report_unit_test_coverage(context):
     """単体テストのカバレッジをレポートする."""
-
-    commands = """
-        coverage erase
-        coverage run --omit */src/test/* /app/src/test/python/myapp/utilities/test_application_version.py
-        coverage report
-        coverage html --directory=./target/docs/coverage/
-    """.strip().split("\n")
-
-    for command in commands:
-        context.run(command)
+    coverage = Coverage()
+    coverage.set_option("run:branch", True)
+    coverage.erase()
+    coverage.start()
+    _run_unit_tests()
+    coverage.stop()
+    coverage.save()
+    coverage.report(include="*/src/main/*")
+    coverage.html_report(include="*/src/main/*", directory="./target/docs/coverage")
 
