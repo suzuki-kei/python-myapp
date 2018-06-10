ce6bf63349a02d3d8c9c3300f7888242742217e8
SUZUKI Kei <todokimasen@gmail.com>
2018-06-10 22:55:09 +0900

    ディレクトリ構成を変更.

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 from os import path
 from invoke import Collection
-from myapp.utilities.modules import import_modules
+from lang.modules import import_modules
 
 namespace = Collection(*import_modules(path.join(path.dirname(__file__), "tasks")))
 
