15202b4243a3fd4db9d07120985438591be4e318
SUZUKI Kei <todokimasen@gmail.com>
2018-10-10 08:02:44 +0900

    ファイルパスではなくモジュール名を指定して実行するように変更.

--- src/tasks/app.py
+++ src/tasks/app.py
@@ -5,7 +5,7 @@ def run(context):
     """
         hello を実行する.
     """
-    context.run("python /opt/myapp/current/src/main/myapp/hello/main.py")
+    context.run("python -m myapp.hello.main")
 
 @task
 def run_webapi(context):
