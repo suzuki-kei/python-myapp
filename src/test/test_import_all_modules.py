d776e38f00725139b1743cb8ae15bc20181c85ae
SUZUKI Kei <todokimasen@gmail.com>
2018-10-14 01:19:28 +0900

    全モジュールをインポートするテストを作成.

---
+++ src/test/test_import_all_modules.py
@@ -0,0 +1,11 @@
+import unittest
+
+from lang.modules import import_modules
+
+class ImportAllModulesTestCase(unittest.TestCase):
+
+    def test(self):
+        # 全てのモジュールがインポート可能であることをテストするとともに,
+        # カバレッジ取得時に全モジュールが結果に含まれるようにするためにインポートする.
+        self.assertEqual(True, import_modules("src/main") is not None)
+
