60ffae253e8a3f966ab87b54172e5d89f79c654f
SUZUKI Kei <todokimasen@gmail.com>
2018-10-15 08:04:48 +0900

    設定ファイルを python-box で扱うように実装.

--- src/main/myapp/hello/main.py
+++ src/main/myapp/hello/main.py
@@ -1,4 +1,30 @@
+import mysql.connector
+
+from myapp.utilities.config import load_config
+
+def connect(**connection_parameters):
+    return mysql.connector.connect(**connection_parameters)
+
+def print_columns(connection):
+    sql = """
+        SELECT
+            *
+        FROM
+            information_schema.COLUMNS
+        ORDER BY
+            ORDINAL_POSITION
+    """
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(sql)
+    for row in cursor.fetchall():
+        print(row)
+
+def main():
+    database_config = load_config("config/database.yml").development
+    connection = connect(**database_config)
+    print_columns(connection)
+    connection.close()
 
 if __name__ == "__main__":
-    print("OK")
+    main()
 
---
+++ src/main/myapp/utilities/config.py
@@ -0,0 +1,14 @@
+import yaml
+import box
+
+def _load_yaml_file(file_path):
+    with open(file_path, "r") as file:
+        return yaml.load(file)
+
+def load_config(file_path):
+    return box.Box(
+            _load_yaml_file(file_path),
+            frozen_box=True,
+            default_box=True,
+    )
+
